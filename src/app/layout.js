4c176ea2613cfdd5ae85dd8ea6a5b1e9b5c5a92d
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2026-01-27 19:18:17 +0600

    creat project structure using next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
+import Sidebar from "@/components/layout/Sidebar";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,10 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
+
+        <section className="max-w-[1320px] mx-auto flex  ">
+
+          <aside  className="w-[508px] ">
+              <Sidebar/>
+          </aside>
+
+           <main className="">
         {children}
+
+           </main>
+
+        </section>
+
       </body>
     </html>
   );
---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <section className='w-full'>
+          <header className=' w-full h-10 bg-[rgba(18,20,20,1)]'>
+
+          </header>
+
+              <main>
+
+              </main>
+    </section>
+
+  )
+}
